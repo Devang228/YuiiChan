271d3f174e139c89388cac2e4b91d33be3735ad6
Devang228 <77545306+Devang228@users.noreply.github.com>
2021-02-15 11:33:02 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -200,9 +200,9 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-oko = TelegramClient("LightYagami", API_ID, API_HASH)
+oko = TelegramClient("Scarlett", API_ID, API_HASH)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
-pbot = Client("LightYagami", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("Scarlett", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
